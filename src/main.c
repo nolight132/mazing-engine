b12a14e533fa4d43c350cfc7768ba54db47b49b3
pavelolizko <olizkopavel@gmail.com>
2024-11-16 14:20:29 +0100

    Remove unnecessary performance measurements

--- src/main.c
+++ src/main.c
@@ -8,20 +8,12 @@ void free2DArray(int **array, int rows);
 
 int main()
 {
-    // clock_t start = clock();
-
     srand(time(NULL));
     int size = 33;
     int **map = generateMaze(size);
     printMap(map, size);
 
     free2DArray(map, size);
-    // clock_t end = clock();
-
-    // double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-
-    // printf("Took %fs\n", time_spent);
-
     return 0;
 }
 
