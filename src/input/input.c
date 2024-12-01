e4634ed77c9cdde07db77672adb43aea4617215e
pavelolizko <olizkopavel@gmail.com>
2024-12-02 00:30:49 +0100

    Fix a bug in the input module

--- src/input/input.c
+++ src/input/input.c
@@ -11,8 +11,8 @@ void handleInput(char input, Camera *camera, double deltaTime)
         if (input == 's')
             camera->position.z -= 2.0f * deltaTime;
         if (input == 'a')
-            camera->position.x -= 2.0f * deltaTime;
-        if (input == 'd')
             camera->position.x += 2.0f * deltaTime;
+        if (input == 'd')
+            camera->position.x -= 2.0f * deltaTime;
     }
 }
