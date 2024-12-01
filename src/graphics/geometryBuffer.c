ebc98171fa0bb45cc5b58f2b3de73f4e54e0ab69
pavelolizko <olizkopavel@gmail.com>
2024-12-01 14:07:24 +0100

    Implement vertical merging

--- src/graphics/geometryBuffer.c
+++ src/graphics/geometryBuffer.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <types.h>
 
@@ -15,18 +16,27 @@ AABB *generateAABBs(int **maze, int size, int *aabbCount)
                 AABB candidate;
                 candidate.min = (Vector3){0, x, z};
                 candidate.max = (Vector3){2, (x + 1), (z + 1)};
-                if (*aabbCount > 0 && aabbs[*aabbCount - 1].min.z == candidate.min.z &&
-                    maze[(int)candidate.min.z][(int)candidate.min.x - 1] == WALL)
+                if (aabbs[*aabbCount - 1].min.z == candidate.min.z && maze[(int)candidate.min.z][(int)candidate.min.x - 1] == WALL)
                 { // Merge AABBs horizontally
                     aabbs[*aabbCount - 1].max = candidate.max;
-                    continue;
+                }
+                else if (z > 0 && maze[z - 1][x] == WALL && maze[z - 1][x + 1] != WALL && maze[z - 1][x - 1] != WALL)
+                { // Merge AABBs vertically
+                    for (int i = 1; i < *aabbCount; i++)
+                    {
+                        if (aabbs[i].max.z == candidate.max.z - 1 && aabbs[i].max.x == candidate.max.x)
+                        {
+                            aabbs[i].max = candidate.max;
+                            break;
+                        }
+                    }
                 }
                 else
                 {
                     aabbs = (AABB *)realloc(aabbs, sizeof(AABB) * (*aabbCount + 1));
                     aabbs[*aabbCount] = candidate;
+                    (*aabbCount)++;
                 }
-                (*aabbCount)++;
             }
         }
     }
